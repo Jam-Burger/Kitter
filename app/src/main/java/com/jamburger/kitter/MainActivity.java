8eb25bcf1ce791446f63c2e6a643dc5b1563a406
Jam-Burger <jay40793@gmail.com>
2023-01-13 22:27:25 +0530

    initial commit

---
+++ app/src/main/java/com/jamburger/kitter/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jamburger.kitter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
