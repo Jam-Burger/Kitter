f332b581cc69c7a7d6d92b7c891e7fcf9bcc49a3
Jam-Burger <jay40793@gmail.com>
2023-02-01 10:24:02 +0530

    implement search fragment
    Signed-off-by: Jam-Burger <jay40793@gmail.com>

---
+++ app/src/main/java/com/jamburger/kitter/adapters/ProfileAdapter.java
@@ -0,0 +1,64 @@
+package com.jamburger.kitter.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.jamburger.kitter.R;
+import com.jamburger.kitter.components.User;
+
+import java.util.List;
+
+public class ProfileAdapter extends RecyclerView.Adapter<ProfileAdapter.ViewHolder> {
+    Context mContext;
+    List<User> mUsers;
+
+    public ProfileAdapter(Context mContext, List<User> mUsers) {
+        this.mContext = mContext;
+        this.mUsers = mUsers;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.profile_item, parent, false);
+        return new ProfileAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        User user = mUsers.get(position);
+        Glide.with(mContext).load(user.getProfileImageUrl()).into(holder.profileImage);
+        holder.username.setText(user.getUsername());
+        holder.name.setText(user.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUsers.size();
+    }
+
+    public void filterList(List<User> filteredList) {
+        mUsers = filteredList;
+        notifyDataSetChanged();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView profileImage;
+        TextView username, name;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            profileImage = itemView.findViewById(R.id.img_profile);
+            username = itemView.findViewById(R.id.txt_username);
+            name = itemView.findViewById(R.id.txt_name);
+        }
+    }
+}
--- app/src/main/java/com/jamburger/kitter/fragments/SearchFragment.java
+++ app/src/main/java/com/jamburger/kitter/fragments/SearchFragment.java
@@ -4,22 +4,88 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.TextView;
 
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.jamburger.kitter.R;
+import com.jamburger.kitter.adapters.ProfileAdapter;
+import com.jamburger.kitter.components.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchFragment extends Fragment {
+    SearchView searchbar;
+    RecyclerView recyclerViewProfiles;
+    TextView messageText;
+    ProfileAdapter profileAdapter;
+    List<User> profiles, allProfiles;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(requireContext(), "In Development", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        recyclerViewProfiles = view.findViewById(R.id.recyclerview_profiles);
+        searchbar = view.findViewById(R.id.et_search);
+        messageText = view.findViewById(R.id.txt_message);
+
+        profiles = new ArrayList<>();
+        allProfiles = new ArrayList<>();
+        profileAdapter = new ProfileAdapter(requireContext(), profiles);
+        recyclerViewProfiles.setHasFixedSize(true);
+        recyclerViewProfiles.setAdapter(profileAdapter);
+
+        readProfiles();
+
+        searchbar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filter(newText);
+                return false;
+            }
+        });
+        return view;
+    }
+
+    private void filter(String text) {
+        profiles.clear();
+        messageText.setVisibility(View.GONE);
+        if (text.length() == 0) return;
+        for (User user : allProfiles) {
+            if (user.getName().toLowerCase().contains(text.toLowerCase()) || user.getUsername().toLowerCase().contains(text.toLowerCase())) {
+                profiles.add(user);
+            }
+        }
+        if (profiles.isEmpty()) {
+            messageText.setVisibility(View.VISIBLE);
+        }
+        profileAdapter.filterList(profiles);
+    }
+
+    private void readProfiles() {
+        CollectionReference userReference = FirebaseFirestore.getInstance().collection("Users");
+        userReference.get().addOnSuccessListener(postSnapshots -> {
+            allProfiles.clear();
+            for (DocumentSnapshot userSnapshot : postSnapshots) {
+                User user = userSnapshot.toObject(User.class);
+                allProfiles.add(user);
+            }
+        });
     }
 }
