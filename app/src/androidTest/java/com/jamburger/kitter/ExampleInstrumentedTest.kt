9c1843a7f69ed26cb89e1d354a0f7e6fe7322980
Jam-Burger <jay40793@gmail.com>
2024-06-04 16:11:08 +0530

    convert test files into kotlin
    Signed-off-by: Jam-Burger <jay40793@gmail.com>

--- app/src/androidTest/java/com/jamburger/kitter/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/jamburger/kitter/ExampleInstrumentedTest.kt
@@ -1,26 +1,17 @@
-package com.jamburger.kitter;
+package com.jamburger.kitter
 
-import android.content.Context;
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
 
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
+    fun useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.jamburger.kitter", appContext.getPackageName());
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        Assert.assertEquals("com.jamburger.kitter", appContext.packageName)
     }
 }
--- app/src/test/java/com/jamburger/kitter/ExampleUnitTest.kt
+++ app/src/test/java/com/jamburger/kitter/ExampleUnitTest.kt
@@ -1,17 +1,11 @@
-package com.jamburger.kitter;
+package com.jamburger.kitter
 
-import org.junit.Test;
+import org.junit.Assert
+import org.junit.Test
 
-import static org.junit.Assert.*;
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-public class ExampleUnitTest {
+class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    fun addition_isCorrect() {
+        Assert.assertEquals(4, (2 + 2).toLong())
     }
 }
